80900b4cb0ed25215142689103615f33fe119a23
Abhinand B <abhinand25112001@gmail.com>
2026-02-23 06:59:18 +0000

    middlewares and ratelimit

--- src/app.js
+++ src/app.js
@@ -2,3 +2,13 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use(rateLimit({
+    windowMs: 15 * 60 * 100,
+    limit: 100,
+}))
