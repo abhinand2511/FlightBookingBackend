a357947abd6be01f459688297e4eed5a4e927655
Abhinand B <abhinand25112001@gmail.com>
2026-02-23 07:46:08 +0000

    middleware updated in app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+import { errorHandler } from "./middlewares/error.middleware";
 
 const app = express();
 
 app.use(helmet());
 app.use(cors());
+app.use(errorHandler);
 
 app.use(rateLimit({
     windowMs: 15 * 60 * 100,
